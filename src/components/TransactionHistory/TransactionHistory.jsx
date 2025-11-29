2f1b7698fd9f702dfff63db1b9c3d68ebb00b1d5
nina <dewi@ukr.net>
2025-11-29 15:34:44 +0100

    goit-neo-react-hw-module1

---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./TransactionHistory.module.css";
+
+const TransactionHistory = ({ items }) => {
+  return (
+    <table className={styles.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ id, type, amount, currency }, index) => (
+          <tr key={id} className={index % 2 === 0 ? styles.evenRow : styles.oddRow}>
+            <td className={styles.transactionType}>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default TransactionHistory;
