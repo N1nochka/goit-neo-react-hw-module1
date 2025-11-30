c880c76d8259c351aa7394fcd6b3e4cbed9f6410
nina <dewi@ukr.net>
2025-11-30 12:46:08 +0100

    Remove dist folder, using root deployment

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  base: './', 
-  build: {
-    sourcemap: true,
-    outDir: 'dist',
-  },
+  base: './',
 });
