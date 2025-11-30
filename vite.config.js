c9c477852573f098013ba77a251e000a2accc5d1
nina <dewi@ukr.net>
2025-11-30 14:58:25 +0100

    Add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   base: '/goit-neo-react-hw-module1/',  
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
 });
