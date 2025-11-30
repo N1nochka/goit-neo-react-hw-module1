5d1b9dddd9945e7a349c7c2e17b02ffbb0d40edf
nina <dewi@ukr.net>
2025-11-30 17:37:35 +0100

    goit-neo-react-hw-module1

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/goit-neo-react-hw-module1/",
   plugins: [react()],
-  build: {
-    sourcemap: true,
-  },
 });
