b3fd21fd328e8f37b365df7ffd6d9923e764d33d
nina <dewi@ukr.net>
2025-11-30 17:44:12 +0100

    Fix relative paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  base: "/goit-neo-react-hw-module1/",
   plugins: [react()],
+  base: "./",   
 });
